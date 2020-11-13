cca3cd3da3177aabf93291f107a8d062477516b2
Theo Paris <theoplaysmc@gmail.com>
2020-11-13 20:46:43 +0000

    stuff

--- src/index.ts
+++ src/index.ts
@@ -8,19 +8,33 @@ client.once("ready", () => {
 	console.log("Ready!");
 });
 
-client.on("message", (message) => {
+client.on("message", async (message) => {
 	//this event is fired, whenever the bot sees a new message
-	if (message.mentions.has(message.author)) {
-		logStream.write(
-			JSON.stringify({
-				guild: message.guild.id,
-				content: message.content,
-				mentions: message.mentions.users.map((u) => ({
-					id: u.id,
-					tag: u.tag,
-				})),
-			})
-		);
-	}
+	if (
+		!message.mentions ||
+		!message.mentions.users ||
+		message.mentions.users.size < 1
+	)
+		return;
+	if (message.author.id === client.user.id) return;
+	
+	const log = JSON.stringify({
+		guild: message.guild.id,
+		content: message.cleanContent,
+		mentions: message.mentions.users.map((u) => ({
+			id: u.id,
+			tag: u.tag,
+		})),
+	});
+	logStream.write(log);
+	try {
+		const logGuildChannel = await message.guild.channels.cache
+			.find((c) => c.name.includes("mod-logs"))
+			.fetch();
+		const logChannel: Discord.TextChannel = (await client.channels.cache.find(
+			(c) => c.id === logGuildChannel.id && c.type === "text"
+		)) as Discord.TextChannel;
+		await logChannel.send(log);
+	} catch {}
 });
 client.login(process.env.TOKEN);
