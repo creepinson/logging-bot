82bced48b9c792f490378299bf4a2b4d62aaf002
Theo Paris <theoplaysmc@gmail.com>
2020-11-13 20:34:05 +0000

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log("Hello world!");
+import Discord from "discord.js";
+import fs from "fs";
+const client = new Discord.Client();
+
+const logStream = fs.createWriteStream(`${__dirname}/../mentions.log`);
+
+client.once("ready", () => {
+	console.log("Ready!");
+});
+
+client.on("message", (message) => {
+	//this event is fired, whenever the bot sees a new message
+	if (message.mentions.has(message.author)) {
+		logStream.write(
+			JSON.stringify({
+				guild: message.guild.id,
+				content: message.content,
+				mentions: message.mentions.users.map((u) => ({
+					id: u.id,
+					tag: u.tag,
+				})),
+			})
+		);
+	}
+});
+client.login(process.env.TOKEN);
